5e29441dfa4c07dbce097deacaff4bc75a4466fb
Ashif Muhammed <ashifmohammed231@gmail.com>
2026-01-22 09:29:05 +0530

    add Jest configuration and tests for Users component

---
+++ components/ui/User.test.jsx
@@ -0,0 +1,44 @@
+import { render, screen, act } from "@testing-library/react";
+import { Suspense } from "react";
+import Users from "./Users";
+
+// Mock notFound to THROW (realistic behavior)
+jest.mock("next/navigation", () => ({
+  notFound: jest.fn(() => {
+    throw new Error("NEXT_NOT_FOUND");
+  }),
+}));
+
+describe("<Users />", () => {
+  it("renders users when data exists", async () => {
+    const usersPromise = Promise.resolve([
+      { id: 1, name: "Ashif" },
+      { id: 2, name: "John" },
+    ]);
+
+    await act(async () => {
+      render(
+        <Suspense fallback={<div>Loading...</div>}>
+          <Users users={usersPromise} />
+        </Suspense>,
+      );
+    });
+    console.log(screen);
+    expect(screen.getByText("Ashif")).toBeInTheDocument();
+    expect(screen.getByText("John")).toBeInTheDocument();
+  });
+
+  it("throws notFound when users list is empty", async () => {
+    const emptyUsersPromise = Promise.resolve([]);
+
+    await expect(async () => {
+      await act(async () => {
+        render(
+          <Suspense fallback={<div>Loading...</div>}>
+            <Users users={emptyUsersPromise} />
+          </Suspense>,
+        );
+      });
+    }).rejects.toThrow("NEXT_NOT_FOUND");
+  });
+});
